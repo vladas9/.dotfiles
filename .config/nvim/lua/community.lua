cc7aadc4b1568443d004316af9fd01205ae5bf73
vladas9 <vladamza63@gmail.com>
2024-09-19 22:54:43 +0300

    first commit

---
+++ .config/nvim/lua/community.lua
@@ -0,0 +1,14 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+	"AstroNvim/astrocommunity",
+	-- { import = "astrocommunity.recipes.telescope-nvchad-theme" },
+	-- { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
+	{ import = "astrocommunity.colorscheme.gruvbox-baby" },
+	{ import = "astrocommunity.media.cord-nvim" },
+}
